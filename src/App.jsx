872e7b3ba85754893146f792e93a0c87af741b41
g0dlessjs <juangalarce.dev@gmail.com>
2026-05-31 17:13:16 -0400

    refactor: split app into reusable React components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { Footer } from "./components/Footer";
+import { Guitar } from "./components/Guitar";
+import { Header } from "./components/Header";
+
 function App() {
   return (
     <>
-      <h1>HolaMundo!</h1>
+      <Header />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          <Guitar />
+          <Guitar />
+          <Guitar />
+        </div>
+      </main>
+
+      <Footer />
     </>
   );
 }
-
 export default App;
